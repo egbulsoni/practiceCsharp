7792f0e5d46c4a3463c45ca628f2ab55cfa160bd
Eduardo <egbulsoni@gmail.com>
2023-10-05 20:10:18 -0300

    static fields and methods, classes

---
+++ Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practiceCode
+{
+    internal class Person
+    {
+        public string name;
+        public int weight;
+        public static string species = "Human";
+        public static int personCount = 0;
+
+        public Person(string newName = "Bob", int newWeight = 150)
+        {
+            name = newName;
+            weight = newWeight;
+            personCount++;
+        }
+
+        public void Eat(int val)
+        {
+            weight += val;
+        }
+
+        public char FirstInitial()
+        {
+            return name[0];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+using practiceCode;
+
+int myNumber = -150;
+int absoluteValue = Math.Abs(myNumber);
+int maxValue = Math.Max(myNumber, 55);
+
+int[] myArray = new int[5];
+myArray[0] = 8;
+myArray[1] = 6;
+
+
+Console.WriteLine(myArray[0]);
+Console.ReadKey();
+
+int myValue = 0;
+int myOtherValue = 0;
+int forLoopVal = 0;
+
+while (myValue < 10)
+{
+    myValue++;
+    myOtherValue -= 5;
+}
+
+for (int i = 0; i < 5; i++)
+{
+    forLoopVal += 10;
+}
+
+Console.WriteLine(myValue);
+Console.WriteLine(myOtherValue);
+Console.WriteLine(forLoopVal);
+Console.ReadKey();
+
+Person myPerson = new Person("Joe", 80);
+myPerson.name = "Joe";
+
+Console.WriteLine(myPerson.name);
+Console.ReadKey();
+myPerson.Eat(10);
+
+Console.WriteLine(myPerson.weight);
+Console.ReadKey();
+
+Console.WriteLine(myPerson.FirstInitial());
+Console.ReadKey();
+
+Person mySecondPerson = new Person("Nancy", 50);
+Console.WriteLine(mySecondPerson.name);
+Console.WriteLine(mySecondPerson.weight);
+
+Person myDefaultPerson = new Person();
+Console.WriteLine(myDefaultPerson.name);
+Console.WriteLine(myDefaultPerson.weight);
+
+Console.WriteLine(Person.species);
+
+Person myStoopidPerson = new Person("Jeff");
+Console.WriteLine(myStoopidPerson.name);
+Console.WriteLine(myStoopidPerson.weight);
+Console.WriteLine(Person.personCount);
